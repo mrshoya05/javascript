9c100c9aac497e7fc10afedd936c7b1d0a1d80ee
shoyab khan <129213723+mrshoya05@users.noreply.github.com>
2023-09-21 22:01:23 +0530

    Add files via upload
    learn javascript programs using condition statements !

---
+++ script.js
@@ -0,0 +1,28 @@
+let  input;
+
+input = "5";
+if(input === "1"){
+        document.write("Monday");
+}else if(input === "2"){
+    document.write("Tuesday");
+}
+else if(input === "3"){
+    document.write("Wednesday");
+}
+else if(input == 4){
+    document.write("Thursday");
+
+}
+else if(input === "5"){
+    document.write("Friday");
+}
+else if(input === "6"){
+    document.write("Saturday");
+}
+else if(input === "7"){
+    document.write("Sunday");
+
+}
+else{
+    document.write("Invalid entry");
+}
